e7e99839eb513a52ec5a9c116650d8033668827a
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-21 23:26:26 +0300

    Expanding README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Toy Problems Requisite
 ## Generating student grades in JavaScript using function
-## using functin declaratin and retuning values while entering the correct parameters
-Student Grade Generator (Toy Problem)-the code executes these. 
+## using functin declaratin and returning values while entering the correct parameters
 Functin prompts the user to input students mark between 0 to 100.
+The getStudentGrade() function prompts the user to input a student's mark and then checks whether the input is valid. If the input is not between 0 and 100, the function returns an error message. Otherwise, it checks the mark against each grade boundary and returns the corresponding grades. 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//Solution for generating students grades
+//write a function generateStudent
 function generateStudentGrade() {
     const marks = parseInt(prompt("Enter student marks (between 0 and 100):"));
-    
+//using condition and return statement 
     if (isNaN(marks) || marks < 0 || marks > 100) {
       return "Invalid input";
     } else if (marks > 79) {
@@ -15,3 +17,24 @@ function generateStudentGrade() {
       return "E";
     }
   }
+
+//Code solution for checking speed
+//using function declaration and expression
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerDemeritPoint = 5;
+  let demeritPoints = 0;
+//conditional statement(using if)
+  if (speed <= speedLimit) {
+    console.log("Ok");
+    return;
+  }
+  demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+
+  if (demeritPoints > 12) {
+    console.log("License suspended");
+  } else {
+    console.log(`Points: ${demeritPoints}`);
+  }
+}
+
